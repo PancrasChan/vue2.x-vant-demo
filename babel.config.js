5eda94850e346624e001feca5b3335e0245b8ccf
PancrasChan <skara_095@163.com>
2021-04-11 21:29:03 +0800

    添加vant定制主题配置文件

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  // vant按需加载
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "vant",
+        libraryDirectory: "es",
+        // 指定样式路径
+        //style: (name) => `${name}/style/less`,
+        style: true,
+      },
+      "vant",
+    ],
+  ],
 };
