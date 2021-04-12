aaf8e4140ba91e4929131bd36cae61ead00ebf1c
PancrasChan <skara_095@163.com>
2021-04-12 11:30:16 +0800

    去掉了store/modules/user.js中的console调试

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -11,9 +11,7 @@ const user = {
   },
   mutations: {
     [SET_USER_INFO](state, user) {
-      console.log("传入的用户信息是: ", user);
       state.userInfo = user;
-      console.log("0003用户信息是: ", state.userInfo.username);
     },
   },
   actions: {
@@ -23,16 +21,13 @@ const user = {
           .then((response) => {
             if (response.code === 200) {
               let result = response.result;
-              console.log("001用户信息是", user.state.userInfo.username);
               commit(SET_USER_INFO, result);
-              console.log("002用户信息是", user.state.userInfo.username);
               resolve(response);
             } else {
               reject(response);
             }
           })
           .catch((error) => {
-            console.log("报错了啊", error);
             reject(error);
           });
       });
